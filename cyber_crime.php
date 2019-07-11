dfd6887d6483090a12afed049c4f565071c2cbd8
kishor-soni <kishoresoni17@gmail.com>
2019-07-11 12:20:13 +0530

    Uploading file

---
+++ cyber_crime.php
@@ -0,0 +1,400 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+
+    <!-- Basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+   
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+ 
+     <!-- Site Metas -->
+    <title>Jabalpur Police</title>   
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Site CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Colors CSS -->
+    <link rel="stylesheet" href="css/colors.css">
+    <!-- ALL VERSION CSS -->
+    <link rel="stylesheet" href="css/versions.css">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!-- Modernizer for Portfolio -->
+    <script src="js/modernizer.js"></script>
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body class="host_version"> 
+
+	<!-- Modal -->
+	<!--div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	  <div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header tit-up">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title">Customer Login</h4>
+			</div>
+			<div class="modal-body customer-box row">
+				<div class="col-md-12">
+					<!-- Nav tabs ->
+					<ul class="nav nav-tabs">
+						<li class="active"><a href="#Login" data-toggle="tab">Login</a></li>
+						<li><a href="#Registration" data-toggle="tab">Registration</a></li>
+					</ul>
+					<!-- Tab panes ->
+					<div class="tab-content">
+						<div class="tab-pane active" id="Login">
+							<form role="form" class="form-horizontal">
+							<div class="form-group">
+								<div class="col-sm-12">
+									<input class="form-control" id="email1" placeholder="Name" type="text">
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-sm-12">
+									<input class="form-control" id="exampleInputPassword1" placeholder="Email" type="email">
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-10">
+									<button type="submit" class="btn btn-light btn-radius btn-brd grd1">
+										Submit
+									</button>
+									<a class="for-pwd" href="javascript:;">Forgot your password?</a>
+								</div>
+							</div>
+							</form>
+						</div>
+						<div class="tab-pane" id="Registration">
+							<form role="form" class="form-horizontal">
+							<div class="form-group">
+								<div class="col-sm-12">
+									<input class="form-control" placeholder="Name" type="text">
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-sm-12">
+									<input class="form-control" id="email" placeholder="Email" type="email">
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-sm-12">
+									<input class="form-control" id="mobile" placeholder="Mobile" type="email">
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-sm-12">
+									<input class="form-control" id="password" placeholder="Password" type="password">
+								</div>
+							</div>
+							<div class="row">							
+								<div class="col-sm-10">
+									<button type="button" class="btn btn-light btn-radius btn-brd grd1">
+										Save &amp; Continue
+									</button>
+									<button type="button" class="btn btn-light btn-radius btn-brd grd1">
+										Cancel</button>
+								</div>
+							</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	  </div>
+	</div-->
+    <header class="header header_style_01">
+        <nav class="megamenu navbar navbar-default">
+            
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+					<!--h3 style="text-align:center;">Jabalpur Police</h3-->
+                    <a  href="index.html"><img src="images/logos/jabalpur_logo1.png" alt="image"></a>
+					
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a  href="index.html" style="font-size:15px;">Home</a></li>
+                        <li><a href="about.html" style="font-size:15px;">About Us </a></li>
+                        <li><a href="mission.html" style="font-size:15px;">Our Mission</a></li>
+                        <li><a href="code_red.html" style="font-size:15px;">Code Red</a></li>
+                        <li><a href="cyber_crime.html" style="font-size:15px;">Cyber Crime</a></li>
+                        <li><a href="press_reference.html" style="font-size:15px;">Press Reference</a></li>
+						<li><a href="gallery.html" style="font-size:15px;"> Gallery</a></li>
+						<li><a href="inform_us.html" style="font-size:15px;">Inform Us</a></li>
+                        <li><a href="contact_us.html" style="font-size:15px;">Contact Us</a></li>
+                    </ul>
+                    <!--ul class="nav navbar-nav navbar-right">
+                        <li><a class="btn-light btn-radius btn-brd log" href="#" data-toggle="modal" data-target="#login"><i class="flaticon-padlock"></i> Customer Login</a></li>
+                    </ul-->
+                </div>
+            
+        </nav>
+    </header>
+    <!-- LOADER -->
+	<div id="preloader">
+		<div class="loading">
+			<div class="finger finger-1">
+				<div class="finger-item">
+				<span></span><i></i>
+				</div>
+			</div>
+  			<div class="finger finger-2">
+				<div class="finger-item">
+				<span></span><i></i>
+				</div>
+			</div>
+  			<div class="finger finger-3">
+				<div class="finger-item">
+				  <span></span><i></i>
+				</div>
+			</div>
+  			<div class="finger finger-4">
+				<div class="finger-item">
+				<span></span><i></i>
+				</div>
+			</div>
+  			<div class="last-finger">
+				<div class="last-finger-item"><i></i></div>
+			</div>
+		</div>
+	</div>
+	<!-- END LOADER -->
+
+	
+	<div class="all-title-box">
+		<div class="container text-center">
+			<h1>Cyber Crime <span class="m_1"></span></h1>
+		</div>
+	</div>
+	
+    <div id="testimonial-box" class="section wb">
+        <div class="container">
+			<div class="row">
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="testimonial clearfix">
+						<div class="desc">
+							<h3><i class="fa fa-quote-left"></i> Wonderful Support!</h3>
+							<p class="lead">They have got my project on time with the competition with a sed highly skilled, and experienced & professional team.</p>
+						</div>
+						<div class="testi-meta">
+							<img src="uploads/testi_01.png" alt="" class="img-responsive alignleft">
+							<h4>James Fernando <small>- Manager of Racer</small></h4>
+						</div>
+						<!-- end testi-meta -->
+					</div>
+					<!-- end testimonial -->
+				</div>
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="testimonial clearfix">
+						<div class="desc">
+							<h3><i class="fa fa-quote-left"></i> Awesome Services!</h3>
+							<p class="lead">Explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you completed.</p>
+						</div>
+						<div class="testi-meta">
+							<img src="uploads/testi_02.png" alt="" class="img-responsive alignleft">
+							<h4>Jacques Philips <small>- Designer</small></h4>
+						</div>
+						<!-- end testi-meta -->
+					</div>
+					<!-- end testimonial -->
+				</div>
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="testimonial clearfix">
+						<div class="desc">
+							<h3><i class="fa fa-quote-left"></i> Great & Talented Team!</h3>
+							<p class="lead">The master-builder of human happines no one rejects, dislikes avoids pleasure itself, because it is very pursue pleasure. </p>
+						</div>
+						<div class="testi-meta">
+							<img src="uploads/testi_03.png" alt="" class="img-responsive alignleft">
+							<h4>Venanda Mercy <small>- Newyork City</small></h4>
+						</div>
+						<!-- end testi-meta -->
+					</div>
+					<!-- end testimonial -->
+				</div>
+			</div>
+			<hr class="hr3">
+			<div class="row">
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="testimonial clearfix">
+						<div class="desc">
+							<h3><i class="fa fa-quote-left"></i> Wonderful Support!</h3>
+							<p class="lead">They have got my project on time with the competition with a sed highly skilled, and experienced & professional team.</p>
+						</div>
+						<div class="testi-meta">
+							<img src="uploads/testi_01.png" alt="" class="img-responsive alignleft">
+							<h4>James Fernando <small>- Manager of Racer</small></h4>
+						</div>
+						<!-- end testi-meta -->
+					</div>
+					<!-- end testimonial -->
+				</div>
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="testimonial clearfix">
+						<div class="desc">
+							<h3><i class="fa fa-quote-left"></i> Awesome Services!</h3>
+							<p class="lead">Explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you completed.</p>
+						</div>
+						<div class="testi-meta">
+							<img src="uploads/testi_02.png" alt="" class="img-responsive alignleft">
+							<h4>Jacques Philips <small>- Designer</small></h4>
+						</div>
+						<!-- end testi-meta -->
+					</div>
+					<!-- end testimonial -->
+				</div>
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="testimonial clearfix">
+						<div class="desc">
+							<h3><i class="fa fa-quote-left"></i> Great & Talented Team!</h3>
+							<p class="lead">The master-builder of human happines no one rejects, dislikes avoids pleasure itself, because it is very pursue pleasure. </p>
+						</div>
+						<div class="testi-meta">
+							<img src="uploads/testi_03.png" alt="" class="img-responsive alignleft">
+							<h4>Venanda Mercy <small>- Newyork City</small></h4>
+						</div>
+						<!-- end testi-meta -->
+					</div><!-- end testimonial -->
+				</div>
+			</div>
+		</div>
+    </div><!-- end section -->
+
+    <div class="parallax section db parallax-off" style="background-image:url('uploads/parallax_02.jpg');">
+        <div class="container">
+            <div class="row logos">
+                <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+                    <a href="#"><img src="uploads/logo_01.png" alt="" class="img-repsonsive"></a>
+                </div>
+                <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+                    <a href="#"><img src="uploads/logo_02.png" alt="" class="img-repsonsive"></a>
+                </div>
+                <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+                    <a href="#"><img src="uploads/logo_03.png" alt="" class="img-repsonsive"></a>
+                </div>
+                <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+                    <a href="#"><img src="uploads/logo_04.png" alt="" class="img-repsonsive"></a>
+                </div>
+                <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+                    <a href="#"><img src="uploads/logo_05.png" alt="" class="img-repsonsive"></a>
+                </div>
+                <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+                    <a href="#"><img src="uploads/logo_06.png" alt="" class="img-repsonsive"></a>
+                </div>
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </div><!-- end section -->
+
+    <section class="section nopad cac text-center">
+        <a href="#"><h3>Interesting our awesome web design services? Just drop an email to us and get quote for free!</h3></a>
+    </section>
+
+       <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="widget clearfix">
+                        <div class="widget-title">
+                            <img src="images/logos/j12.png" alt="" style="height:100px;width:100px;"><br><br>
+                            <h2 style="color:#fff;">Jabalpur Police</h2>
+                        </div>
+                        <p> Jabalpur district is the southernmost district of Madhaya Pradesh. Its headquarter is Jabalpur City which is an ancient city.</p>
+                        
+                    </div><!-- end clearfix -->
+                </div><!-- end col -->
+
+				<div class="col-md-3 col-sm-3 col-xs-12">
+                    <div class="widget clearfix">
+                        <div class="widget-title">
+                            <h3>Information Link</h3>
+                        </div>
+                        <ul class="footer-links">
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="about.html">About Us</a></li>
+                            <li><a href="mission.html">Our Mission</a></li>
+							<li><a href="cyber_crime.html">Cyber Crime </a></li>
+							<li><a href="press_reference.html">Press Reference</a></li>
+							<li><a href="contact_us.html">Contact</a></li>
+                        </ul><!-- end links -->
+                    </div><!-- end clearfix -->
+                </div><!-- end col -->
+				
+                <div class="col-md-3 col-sm-3 col-xs-12">
+                    <div class="widget clearfix">
+                        <div class="widget-title">
+                            <h3>Contact Details</h3>
+                        </div>
+
+                        <ul class="footer-links">
+                            <li><a href="mailto:#">jabalpurpolice@gmail.com</a></li>
+                            <li><a href="#">www.jabalpolice.com</a></li>
+                            <li>Civil Line Police,Jabalpur</li>
+                            <li>+61 3 8376 6284</li>
+                        </ul><!-- end links -->
+                    </div><!-- end clearfix -->
+                </div><!-- end col -->
+
+                <div class="col-md-2 col-sm-2 col-xs-12">
+                    <div class="widget clearfix">
+                        <div class="widget-title">
+                            <h3>Social</h3>
+                        </div>
+                        <ul class="footer-links" >
+                            <li><a href="#"><i class="fa fa-facebook" ></i> Facebook</a></li>
+                            <li><a href="#"><i class="fa fa-instagram"></i> Instagram</a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i> Twitter</a></li>
+                            
+                        </ul><!-- end links -->
+                    </div><!-- end clearfix -->
+                </div><!-- end col -->
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </footer><!-- end footer -->
+
+    <div class="copyrights">
+        <div class="container">
+            <div class="footer-distributed">
+                <div class="footer-ceneter" style="text-align:center;">                   
+                    <p class="footer-company-name">All Rights Reserved. &copy; 2019 Jabalpur Police By: <a href="https://www.tendsweb.com">Tends Web</a> </p> 
+                </div>
+
+              
+            </div>
+        </div><!-- end container -->
+    </div><!-- end copyrights -->
+
+    <a href="#" id="scroll-to-top" class="dmtop global-radius"><i class="fa fa-angle-up"></i></a>
+
+    <!-- ALL JS FILES -->
+    <script src="js/all.js"></script>
+    <!-- ALL PLUGINS -->
+    <script src="js/custom.js"></script>
+
+</body>
+</html>
